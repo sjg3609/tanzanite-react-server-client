68732798d9cd8c159619ae3ddf157acaf79f5278
Sam Gossie <sjg3609@gmail.com>
2023-03-14 20:10:37 -0500

    commit from class notes

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,8 +1,33 @@
 // Source in our stylesheet
+// 'import is similar to 'require' on the server
 import './App.css';
+
+// UseState updated the DOM when a variable changes
+import {useState} from 'react';
+import CreatureList from '../CreatureList/CreatureList.jsx';
 
 function App () {
   // Our functions and variables will go here
+  // This is how we previously declared variables
+  // let firstName = 'Sam';
+  const [firstName, setFirstName] = useState('Sam');
+  // let counter = 0;
+
+  // const [myStateVariable, setMyVariable] = useState('initial value');
+
+  //     name     function               default value
+  const [counter, setCounter] = useState(0);
+  // function to increase click counter
+  let increaseClickCounter = () => {
+    // counter +=1; Do not modify state directly!
+    setCounter(counter + 1);
+    console.log(counter);
+  }
+
+  let decreaseClickCounter = () => {
+    setCounter(counter - 1);
+    console.log(counter);
+  }
 
   return (
     // This looks like HTML but is really JSX
@@ -10,6 +35,15 @@ function App () {
       <header>
         <h1>Welcome to React!</h1>
       </header>
+      <p>Hello {firstName}!</p>
+      <p>You clicked {counter} times!</p>
+      <button onClick={increaseClickCounter}>Increase</button>
+      <button onClick={decreaseClickCounter}>Decrease</button>
+
+      <CreatureList/>
+     
+      {/* <input type= "submit" value="Submit"/> */}
+
     </div>
   );
 }
---
+++ src/components/CreatureList/CreatureList.jsx
@@ -0,0 +1,46 @@
+import {useState, useEffect } from 'react';
+// we must import axios in each component we want to use it
+import axios from 'axios';
+
+
+function CreatureList() {
+    const [listOfCreatures, setListOfCreatures] = useState([
+        {
+            name: 'Unicorn',
+            origin: 'Norway'
+        }
+    ]);
+
+    const fetchCreatureList = () => {
+        axios.get('/creature').then((response) => {
+            // Update the array. React does the rest
+            setListOfCreatures(response.data);
+        }).catch((error) => {
+            console.log(`Error in GET ${error}`);
+            alert('Something went wrong!');
+        })
+    }
+
+    useEffect(() => {
+        // at this point React is ready
+        fetchCreatureList();
+    }, []); // ! Remember an empty array at the end
+
+    // All components return what you want them to display
+    return (
+        <div>
+            <h2>Creature List</h2>
+            <ul>
+                {
+                    listOfCreatures.map((creature) => (
+                        // what we want to render
+                        <li>{creature.name} from {creature.origin}</li>
+                    ))
+                }
+            </ul>
+        </div>
+    );
+}
+
+// All components must ecport
+export default CreatureList;
